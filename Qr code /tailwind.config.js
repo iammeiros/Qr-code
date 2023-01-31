48dbb14ca28d211e48386a464ed06c1c26557508
abakar <abakar618@gmail.com>
2023-01-31 21:26:25 +0100

    mobile done

--- Qr code /tailwind.config.js
+++ Qr code /tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightGray: 'hsl(212, 45%, 89%)',
+        grayishBlue: 'hsl(220, 15%, 55%)',
+        darkBlue: 'hsl(218, 44%, 22%)',
+      },
+
+      fontFamily: {
+        outfit: 'outfit',
+      }
+    },
   },
   plugins: [],
 }
